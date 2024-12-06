4fd9adba5b862638b4cb2b6843d75d3645094146
Matvey <matvejlarionovit@gamil.com>
2024-12-06 19:17:39 +0300

    begin to create mainPage

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,11 @@
 import { sendRequest } from "./sendRequest.js"
 const userId = sessionStorage.getItem('id')
-const bodyHtml = sendRequest({ method: "GET", pathname: `main/bodyHtml/${userId}` })
-bodyHtml.then(res=>document.body.innerHTML = res)
+const showMainPage = () => {
+    if (!userId) {
+        window.location = '/'
+        return
+    }
+    const bodyHtml = sendRequest({ method: "GET", pathname: `main/bodyHtml/${userId}` })
+    bodyHtml.then(res => document.body.innerHTML = res)
+}
+showMainPage()
