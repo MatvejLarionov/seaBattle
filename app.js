7d4bddaf7d0300f7ada170871198034af4960da7
Matvey <matvejlarionovit@gamil.com>
2024-12-08 19:01:53 +0300

    begin create connect betwen gamers

--- app.js
+++ app.js
@@ -1,8 +1,58 @@
+const http = require("http")
 const express = require('express')
+const WebSocket = require("ws")
+const app = express()
+
 const signInRouter = require('./routes/signIn')
 const mainPageRouter = require('./routes/mainPage.js')
-const app = express()
 const port = 3000
+
+const server = http.createServer(app)
+const wsServer = new WebSocket.Server({ server })
+wsServer.on("connection", ws => {
+    ws.on("message", message => {
+        const ms = JSON.parse(message)
+        switch (ms.type) {
+            case "setLogin":
+                ws.clientData = { login: ms.login }
+                break;
+            case "request to join":
+                const clients = [...wsServer.clients]
+                const partner = clients.find(item => item.clientData.login === ms.login)
+                if (partner && partner !== ws) {
+                    ws.clientData.partner = partner
+                    partner.clientData.partner = ws
+                    partner.send(JSON.stringify(
+                        {
+                            type: "request to join",
+                            partnerLogin: ws.clientData.login
+                        }
+                    ))
+                }
+                else
+                    ws.send(JSON.stringify({ type: "notFound" }))
+                break;
+            case "acceptJoin":
+                ws.clientData.partner.send(JSON.stringify({
+                    type: "acceptJoin",
+                    partnerLogin: ws.clientData.login
+                }))
+                ws.send(JSON.stringify({
+                    type: "acceptJoin",
+                    partnerLogin: ws.clientData.partner.clientData.login
+                 }))
+                break;
+            case "rejectJoin":
+                ws.clientData.partner.send(JSON.stringify({ type: "rejectJoin" }))
+                ws.clientData.partner.clientData.partner = undefined
+                ws.clientData.partner = undefined
+                break;
+
+            default:
+                break;
+        }
+    })
+})
 
 app.set("view engine", "hbs")
 
@@ -10,4 +60,4 @@ app.use('/signIn', signInRouter)
 app.use("/main", mainPageRouter)
 app.use(express.static('./public'))
 app.use((req, res) => res.status(404).send("<h2>Not found</h2>"))
-app.listen(port, () => console.log(`http://localhost:${port}`))
+server.listen(port, () => console.log(`http://localhost:${port}`))
--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,77 @@
 import { sendRequest } from "./sendRequest.js"
 const userId = sessionStorage.getItem('id')
-const showMainPage = () => {
+const showMainPage = async () => {
     if (!userId) {
         window.location = '/'
         return
     }
-    const bodyHtml = sendRequest({ method: "GET", pathname: `main/bodyHtml/${userId}` })
-    bodyHtml.then(res => document.body.innerHTML = res)
+    const bodyHtml = await sendRequest({ method: "GET", pathname: `main/bodyHtml/${userId}` })
+    document.body.innerHTML = bodyHtml
 }
-showMainPage()
+await showMainPage()
+
+
+const webSocket = new WebSocket(`ws://localhost:3000`)
+
+webSocket.onopen = () => {
+    webSocket.send(JSON.stringify({
+        type: "setLogin",
+        login: login.innerText
+    }))
+}
+
+const btn = document.getElementById('btn')
+const inp = document.getElementById('inpLogin')
+btn.addEventListener('click', (event) => {
+    event.preventDefault()
+    const sendObj = {
+        type: "request to join",
+        login: inp.value
+    }
+    webSocket.send(JSON.stringify(sendObj))
+})
+
+const dialogRequest = document.getElementById("dialogRequest")
+const dialogResponse = document.getElementById("dialogResponse")
+const partnerLogin = document.getElementById("partnerLogin")
+webSocket.onmessage = (e) => {
+    const body = JSON.parse(e.data)
+    switch (body.type) {
+        case "request to join":
+            partnerLogin.innerText = body.partnerLogin
+            dialogRequest.showModal()
+            break;
+        case "acceptJoin":
+            playContainer.style.display = "block"
+            partnerLogin1.innerText = body.partnerLogin
+            form.style.display = "none"
+            break;
+        case "rejectJoin":
+            text.innerText = `${inp.value} отклонил запрос`
+            dialogResponse.showModal()
+            break;
+        case "notFound":
+            text.innerText = `${inp.value} не найден`
+            dialogResponse.showModal()
+            break;
+        default:
+            break;
+    }
+}
+
+accept.addEventListener("click", () => {
+    webSocket.send(JSON.stringify({
+        type: "acceptJoin",
+    }))
+    dialogRequest.close()
+})
+
+reject.addEventListener("click", () => {
+    webSocket.send(JSON.stringify({
+        type: "rejectJoin",
+    }))
+    dialogRequest.close()
+})
+btnClose.addEventListener("click", () => {
+    dialogResponse.close()
+})
