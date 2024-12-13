7762243cdcc2c7aa701620baf6eca3d64ebef7da
Matvey <matvejlarionovit@gamil.com>
2024-12-13 19:33:02 +0300

    optimization fileSystem

--- data/usersData.js
+++ data/usersData.js
@@ -2,31 +2,52 @@
 const fs = require('fs')
 const path = require('path')
 const pathJsons = './data/usersJsons'
+const filesController = {
+    getNumberFile(fileName) {
+        return Number(fileName.split(/\(|\)/)[1])
+    },
+    getFileName(num) {
+        return `users(${num})[1].json`
+    },
+    lockFile(fileName) {
+        const arr = fileName.split(/(\[|\])/)
+        arr[2] = 0
+        const newName = arr.join("")
+        fs.renameSync(path.join(pathJsons, fileName), path.join(pathJsons, newName))
+    },
+    unlockFile(fileName) {
+        const arr = fileName.split(/(\[|\])/)
+        arr[2] = 1
+        const newName = arr.join("")
+        fs.renameSync(path.join(pathJsons, fileName), path.join(pathJsons, newName))
+    },
+    isUnLockFile(fileName) {
+        return fileName.split(/\[|\]/)[1] == 1
+    }
+}
 const usersData = {
+    limitInFile: 10,
     create(user) {
         const names = fs.readdirSync(pathJsons)
-        const unfullFile = names.find(item => {
-            if (item.split(/\[|\]/)[1] == 1)
-                return true;
-        })
+        const unfullFile = names.find(item => filesController.isUnLockFile(item))
         if (unfullFile) {
             const data = JSON.parse(fs.readFileSync(path.join(pathJsons, unfullFile), 'utf8'))
-            if (data.length > 0) {
-                user.id = data.at(-1).id + 1
+            let i = 0
+            for (; i < data.length - 1; i++) {
+                if (data[i + 1].id - data[i].id > 1)
+                    break
             }
-            else
-                user.id = (names.length - 1) * 10
-            data.push(user)
+            user.id = data[i].id + 1
+            console.log(user.id)
+            const index = user.id % this.limitInFile
+            data.splice(index, 0, user)
             const pathFile = path.join(pathJsons, unfullFile)
             fs.writeFileSync(pathFile, JSON.stringify(data))
-            if (data.length >= 10) {
-                const arr = unfullFile.split(/(\[|\])/)
-                arr[2] = 0
-                const newName = arr.join('')
-                fs.renameSync(pathFile, path.join(pathJsons, newName))
+            if (data.length >= this.limitInFile) {
+                filesController.lockFile(unfullFile)
             }
         } else {
-            user.id = names.length * 10
+            user.id = names.length * this.limitInFile
             const pathFile = path.join(pathJsons, `users(${names.length})[1].json`)
             fs.writeFileSync(pathFile, JSON.stringify([user]))
         }
